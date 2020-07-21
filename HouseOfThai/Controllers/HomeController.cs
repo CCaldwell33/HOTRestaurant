7177e99e29b2f2aa1f12806e4612ded891c6f149
Chris Caldwell <ccaldwell@hotmail.com>
2020-07-21 15:57:04 -0400

    initialCheckin from local code

---
+++ HouseOfThai/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HouseOfThai.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return null;
+        }
+    }
+}
